64c066bb646ffae7f219b4255518f8b10fefe8c1
rawnullbyte <andrexyt2012@gmail.com>
2026-03-22 16:08:20 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,23 @@
 # Exit on any error
 set -e
 
-echo "📦 Building Frontend..."
-cd frontend
-npm run build
-cd ..
+skip_build=false
+
+for arg in "$@"; do
+  if [ "$arg" == "--skip-build" ]; then
+    skip_build=true
+  fi
+done
+
+if [ "$skip_build" == true ]; then
+  echo "📦 Skipping Frontend Build..."
+else
+  echo "📦 Building Frontend..."
+  cd frontend
+  rm -R dist
+  npm run build
+  cd ..
+fi
 
 echo "🔥 Launching Backend..."
 pip install -r requirements.txt
